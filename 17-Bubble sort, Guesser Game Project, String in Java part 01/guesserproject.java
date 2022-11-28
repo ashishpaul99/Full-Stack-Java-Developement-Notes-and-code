425728ab79d10356913dc702b0a1c54448f3b1eb
GADDALA ASHISH <ashishpaul1199@gmail.com>
2022-11-28 23:48:40 +0530

    Add files via upload

---
+++ 17-Bubble sort, Guesser Game Project, String in Java part 01/guesserproject.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+
+class Guesser {
+	//has part
+	int guessNum;
+	
+	//do part
+	int guessNum(){
+	//asking guesser to guess number
+		System.out.println("Guesser guess a number:");
+		Scanner scan=new Scanner(System.in);
+	//storing guessed number in variable
+		guessNum=scan.nextInt();
+		return guessNum;
+	}
+}
+
+class Player{
+	//has part
+	int guessNum;
+	//do part
+	int guessNum(){
+		System.out.println("Player guess a number:");
+		Scanner p=new Scanner(System.in);
+		guessNum=p.nextInt();
+		return guessNum;
+	}
+}
+
+class Umpire{
+	//has part
+	int numFromGuesser;
+	int numFromPlayer1;
+	int numFromPlayer2;
+	int numFromPlayer3;
+	//do part
+	void collectNumFromGuesser(){
+//		invoke values from guesser
+		Guesser g=new Guesser();
+		numFromGuesser=g.guessNum();
+	 }
+	
+//	invoke values from Player
+	void collectNumFromPlayers() {
+		Player p1=new Player();
+		numFromPlayer1=p1.guessNum();
+		
+		Player p2=new Player();
+		numFromPlayer2=p2.guessNum();
+		
+		Player p3=new Player();
+		numFromPlayer3=p3.guessNum();
+	  }
+	
+	void compare() {
+		if(numFromGuesser==numFromPlayer1) {
+			if(numFromGuesser==numFromPlayer2 && numFromGuesser==numFromPlayer3) {
+				System.out.println("All player won the match");
+			}
+			else if(numFromGuesser==numFromPlayer2) {
+				System.out.println("Player 1 and 2 won the match");
+			}
+			else if(numFromGuesser==numFromPlayer3) {
+				System.out.println("Player 1 and Player 3 won match");
+			}
+			else {
+			System.out.println("Player 1 Won the match");
+			}
+		}
+		else if(numFromGuesser==numFromPlayer2) {
+			
+			if(numFromGuesser==numFromPlayer3) {
+				System.out.println("Player 2 and Player 3 won the match");
+			}
+			else {
+			System.out.println("Player 2 Won the match");
+			}
+		}
+		else if(numFromGuesser==numFromPlayer3) {
+			System.out.println("Player 3 Won the match");
+		}
+		else {
+			System.out.println("game lost try again");
+		}
+	}
+	
+	
+}
+public class guesserproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   Umpire u=new Umpire();
+   u.collectNumFromGuesser();
+   u.collectNumFromPlayers();
+   u.compare();
+   
+	}
+
+}
