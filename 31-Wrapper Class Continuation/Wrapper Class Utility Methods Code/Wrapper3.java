2b895b0b814acf66ef8127e82d422871a0b4c1b2
GADDALA ASHISH <ashishpaul1199@gmail.com>
2023-02-27 20:01:51 +0530

    Add files via upload

---
+++ 31-Wrapper Class Continuation/Wrapper Class Utility Methods Code/Wrapper3.java
@@ -0,0 +1,77 @@
+//3. paraseXXX()-converts string to primitive type
+//String type -->ParseXXX()--> Primitive type
+//String is object
+public class Wrapper3 {
+
+	public static void main(String[] args) {
+		
+//		form-1 : here string is input.
+//		======
+//	  >>public static primitive parseXXX(String s)
+//	  >>Every wrapper class,except Character class has parseXXX() to convert String into
+//		primitive type.
+		
+//		1. String type to primitive type
+		int i=Integer.parseInt("10");
+		System.out.println(i); //output : 10
+		
+//		2. String type to boolean type
+	//  public static boolean parseBoolean(java.lang.String);
+		boolean b=Boolean.parseBoolean("Ashish");
+		System.out.println(b);//false
+		
+//		2. String type to double type
+		double d1= Double.parseDouble("10.5");
+		System.out.println(d1);//10.5
+		
+//		3. when we give string type input instead of integer type it
+//		Through NumberFormatException
+//		Integer i1=Integer.parseInt("ten");
+//		System.out.println(i1);
+		
+//		form-2 : here input is string and radix
+//		public static primitive parseXXXX(String s, int radix)
+//      => range is from 2 to 36
+				
+				Integer i3=Integer.parseInt("100", 2);
+				System.out.println(i3);//4
+		
+		
+
+		
+
+		
+		
+		
+
+		
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+
+		
+
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
+
+//1. valueOf()- converts primitive type and string to wrapper type
+		//2. xxxvalue() -converts wrapper type to primitive type
+		//3. paraseXXX()-converts string type to primitive type
+
+ 
+
---
+++ 31-Wrapper Class Continuation/Wrapper Class Utility Methods Code/Wrapper4.java
@@ -0,0 +1,49 @@
+
+public class Wrapper4 {
+
+	public static void main(String[] args) {
+		
+//		#Usage of wrapper class in real time coding
+//		commandline arguments ==> String input ==> arg[0],args[1]
+//   ==>write a program to take inputs from the command line and perform arithemetic operation
+//		command line arguments are treated as string
+
+//	==>code-1
+		
+//		System.out.println(args[0]+args[1]);
+		
+//		>>giving arguments from command line
+		
+//		C:\Users\Ashishpaul>cd Desktop
+//		C:\Users\Ashishpaul\Desktop>cd java
+//		C:\Users\Ashishpaul\Desktop\java>javac Test.java
+//		C:\Users\Ashishpaul\Desktop\java>java Test ashish paul
+//		ashishpaul
+		
+//   ==>code-2
+		
+		//Performing operation on commandline arguments
+//		int i1 = Integer.parseInt(args[0]);
+//		int i2 = Integer.parseInt(args[1]);
+//		System.out.println(i1+i2);//30
+//		System.out.println(i1-i2);//-10
+//		System.out.println(i1*i2);//200
+//		System.out.println(i1/i2);//0
+		
+		
+//		>>giving arguments from command line
+		
+		//C:\Users\Ashishpaul\Desktop\java>javac Test1.java
+
+		//C:\Users\Ashishpaul\Desktop\java>java Test1 10 20
+		//30
+		//-10
+		//200
+		//0
+
+		//when we not give any argument it is zero.
+		//argument-->we can process in string or  we can process converting to primitive type.
+
+	}
+
+}
