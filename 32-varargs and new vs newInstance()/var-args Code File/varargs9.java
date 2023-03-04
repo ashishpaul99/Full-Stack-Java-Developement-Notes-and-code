7e438e3cc7b10a3ceed4c2f155df521ab0831287
GADDALA ASHISH <ashishpaul1199@gmail.com>
2023-03-04 11:54:01 +0530

    Add files via upload

---
+++ 32-varargs and new vs newInstance()/var-args Code File/varargs9.java
@@ -0,0 +1,28 @@
+//case2::Widening vs var-arg method
+
+class Demo9{
+	public static void methodOne(long l) {
+		System.out.println("widening");
+
+		}
+		public static void methodOne(int... i) {
+
+		System.out.println("var-arg method");
+
+		}
+}
+
+public class varargs9 {
+
+	public static void main(String[] args) {
+	
+		int x=10;
+		Demo9 d=new Demo9();
+		d.methodOne(x);//(ptimitive type) int-->implicit typecasting--> long(binding happens by  compiler)
+		
+	}
+
+}
+
+//output:
+//	AutoBoxing
