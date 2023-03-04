0a8bd4c12f82b6a9721c256906380814674329c5
GADDALA ASHISH <ashishpaul1199@gmail.com>
2023-03-04 12:38:51 +0530

    Add files via upload

---
+++ 32-varargs and new vs newInstance()/var-args Code File/varargs11.java
@@ -0,0 +1,34 @@
+class Demo11
+{
+	public static void methodOne(Long l) {
+		System.out.println("Long");
+	}
+}
+public class varargs11 {
+
+	public static void main(String[] args) {
+		int x=10;
+		Demo11 d=new Demo11();
+		d.methodOne(x); //CE: can't find the method
+//		1. int =====> implicit type casting(widening)===>long,float,double(match not found)
+//		2. int =====> Autoboxing ===> Integer(match not found) 
+//		then again it has do type casting
+//		3. Integer-->type casting(widening)--> Number,object(parent of integer)
+//		compiler search for number and object.(match not found)
+		
+//		here call will not be resolved because match not found.
+//		it gives compilation error. 
+		//CE: can't find the method
+		
+//		widening on integer--> 
+//		widening- child reference can be collected by parent type which is called as "Implicit typecasting"
+		
+		
+//		Note:
+//			Widening followed by Autoboxing is not allowed in java, but Autoboxing followed by
+//			widening is allowed.
+//		whenever a compiler tries to bind method call first prefernce is given to autoBoxing then widening.
+
+	}
+
+}
