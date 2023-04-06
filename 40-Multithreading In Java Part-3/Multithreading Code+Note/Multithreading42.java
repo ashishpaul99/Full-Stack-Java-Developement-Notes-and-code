d3aedf56e8fd51c084ff8ea4d67e1e9fcbbfea77
GADDALA ASHISH <ashishpaul1199@gmail.com>
2023-04-06 22:05:42 +0530

    Update Multithreading42.java

--- 40-Multithreading In Java Part-3/Multithreading Code+Note/Multithreading42.java
+++ 40-Multithreading In Java Part-3/Multithreading Code+Note/Multithreading42.java
@@ -1,10 +1,19 @@
 //=>Life of thread when sleep() method is called
 //->see picute at 2:43:00
 //->when we call sleep() method then thread goes into sleep 
-//  state.
-//->Possibilty of thread to comming out of sleeping state
+//  state
+// 1. Possibility of thred to going into sleeping state-
+// eg:
+//    a. thread.join(1000);
+//    b. thread.join(1000,100);
+
+// ->by using two method signature of sleep
+
+// 2. Possibilty of thread to comming out of sleeping state
 //1. time expires
 //2. if sleeping thread is intruppted
+
+
                                              
 //                                         
 //                                         thread.join(1000);
