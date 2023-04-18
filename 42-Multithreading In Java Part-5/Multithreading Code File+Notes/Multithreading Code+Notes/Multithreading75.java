c2fbf9fa513bfd7584958a866b12d0d44de3a410
GADDALA ASHISH <ashishpaul1199@gmail.com>
2023-04-18 19:00:30 +0530

    Add files via upload

---
+++ 42-Multithreading In Java Part-5/Multithreading Code File+Notes/Multithreading Code+Notes/Multithreading75.java
@@ -0,0 +1,193 @@
+//=>Making main thread sleep
+//Example-1:making main thread sleep for 3 sec
+//->delay of three seconds giving for main thread.
+//->main thread is sleeping then thread sheduler gives chance
+//  to child thread.
+//->then child thread will do the calculation and immediately 
+//  it will notify for waiting thread(main thread).
+//->but in this case main thread is in sleeping state.
+//         Thread.sleep(3000);
+//->here main thread will not receive the notification 
+//  because it is in sleeping state.
+//->after that main thread wakes up and starts executing.
+//->it calls wait method. 
+//->child thread executed the task and went into dead state
+//because of this reason main thread encounters infinite wait.
+
+//	synchronized(b) {  
+//			System.out.println("Main thread calling wait() method");//step-1
+//			b.wait();     //-->Infinite wait
+//			System.out.println("Main thread got notification call");//step-4
+//			System.out.println(b.total);
+//	}
+
+//->this is infinite wait is called dead lock.
+
+
+//Code
+//class ThreadB extends Thread
+//{
+//
+////  the info in total variable should shared to main thread
+//	int total=0; 
+//	
+//// ->calculating sum of 100 numbers
+//	@Override
+//	public void run() {
+//		synchronized(this) {
+//		System.out.println("Child thread started the calculation");//step-2
+//		for(int i=1;i<=100;i++) {
+//			total+=i;
+//		}
+//		System.out.println("Child thread giving notification call");//step-3
+////  ->notify should called by ThreadB object which is current object.
+//		this.notify(); 
+//	
+//	   }
+//	}
+//}
+//public class Multithreading75 {
+//
+//	public static void main(String[] args) throws InterruptedException {
+//		ThreadB b=new ThreadB();
+//		b.start(); //-->Line-1
+//		
+////		main thread is sleeping
+//		Thread.sleep(3000);
+//		
+////	    two threads are present here->Main thread(5), Child therad(5)	
+////		main thread should wait on b
+//		synchronized(b) {  //-->Line-2
+//			System.out.println("Main thread calling wait() method");//step-1
+//			b.wait();     //-->Line-3
+//			System.out.println("Main thread got notification call");//step-4
+//			System.out.println(b.total);
+//		}
+//	}
+//}
+
+
+//output:
+//	Child thread started the calculation
+//	Child thread giving notification call
+//	Main thread calling wait() method
+
+
+
+//Example-2:making main thread wait for 1 sec
+
+//Code:
+//class ThreadB extends Thread
+//{
+//
+////  the info in total variable should shared to main thread
+//	int total=0; 
+//	
+//// ->calculating sum of 100 numbers
+//	@Override
+//	public void run() {
+//		synchronized(this) {
+//		System.out.println("Child thread started the calculation");
+//		for(int i=1;i<=100;i++) {
+//			total+=i;
+//		}
+//		System.out.println("Child thread giving notification call");
+////  ->notify should called by ThreadB object which is current object.
+//		this.notify(); 
+//	
+//	   }
+//	}
+//}
+//public class Multithreading75 {
+//
+//	public static void main(String[] args) throws InterruptedException {
+//		ThreadB b=new ThreadB();
+//		b.start(); 
+//		
+////		main thread is sleeping
+//		Thread.sleep(3000);
+//		
+////	    two threads are present here->Main thread(5), Child therad(5)	
+////		main thread should wait on b
+//		synchronized(b) { 
+//			System.out.println("Main thread calling wait() method");
+////		making main thread for 1 sec
+//			b.wait(1000); //  
+//			System.out.println("Main thread got notification call");
+//			System.out.println(b.total);
+//		}
+//	}
+//}
+
+//ouput:
+//	Child thread started the calculation
+//	Child thread giving notification call
+//	Main thread calling wait() method
+//	Main thread got notification call
+//	5050
+
+//Example-3:making main thread wait for 1 sec and child thread sleep for 
+//3 sec
+//->main thread want notification in 1 sec.
+//->child notifies after 3 sec.
+//->notifying main thread after 3 sec after executing task
+//->Main thread is another waiting state and waits for lock
+//  once it gets lock it start executing.
+//->
+
+//Code:
+class ThreadB extends Thread
+{
+
+//  the info in total variable should shared to main thread
+	int total=0; 
+	
+// ->calculating sum of 100 numbers
+	@Override
+	public void run() {
+		synchronized(this) {
+		System.out.println("Child thread started the calculation");
+		for(int i=1;i<=100;i++) {
+			total+=i;
+		}
+		
+//		notifying main thread after 3 sec od executing task
+		try {
+			Thread.sleep(3000);
+		}
+		catch(Exception e) {
+			
+		}
+		System.out.println("Child thread giving notification call");
+//  ->notify should called by ThreadB object which is current object.
+		this.notify(); 
+	
+	   }
+	}
+}
+public class Multithreading75 {
+
+	public static void main(String[] args) throws InterruptedException {
+		ThreadB b=new ThreadB();
+		b.start(); 
+		
+		
+//	    two threads are present here->Main thread(5), Child therad(5)	
+//		main thread should wait on b
+		synchronized(b) { 
+			System.out.println("Main thread calling wait() method");
+//		making main thread for 1 sec
+			b.wait(1000); //  
+			System.out.println("Main thread got notification call");
+			System.out.println(b.total);
+		}
+	}
+}
+
+//output:
+//	Main thread calling wait() method
+//	Child thread started the calculation
+//	Child thread giving notification call
+//	Main thread got notification call
+//	5050
+
---
+++ 42-Multithreading In Java Part-5/Multithreading Code File+Notes/Multithreading Code+Notes/Multithreading76.java
@@ -0,0 +1,190 @@
+//=>Producer and Consumer Problem Example to understand 
+//  synchronization
+
+//->see pictue at 2:25:00
+//                          _________
+//                         |         |
+//   Producer------------> |  item   |<-----------consumer
+//    notify()             |_________|             wait()
+//->there is one queue.
+//->refernce of the object is q
+//->there is one thread called Producer.
+//->there is another thread called consumer.
+//->Producer produce the item.
+//->Cosumer consumes the item.
+//->consumer waits for the item until it produce.
+//->producer notify's when the item is produced.
+//->wait() and notify() method is invoked on Queue object.
+//->Queue is in built class in java.
+//->if method has to available for every class then it should
+//  come from its parent.
+//->Object is parent for every class in java. 
+
+//Code explaination:
+//->produce the item to the queue and notify to the consumer.
+//->consumer thread wait if queue object is empty and
+//  when the item comes to queue it has to come out of 
+//  waiting state.
+
+
+
+
+//Producer => produce the item and update in the Queue
+//Consumer => consume the item from the Queue
+
+//class Producer extends Thread{
+//		Producer(){
+//			synchronized(q){
+//				produce the item and update it to queue
+//				q.notify();
+//			}
+//		}
+//	}
+//	class Consumer extends Thread{
+//		Consumer(){
+//	synchronized(q){
+//		if(q is empty){
+//			q.wait();
+//		}else{
+////			consume the item from the queue
+//		}
+//	}
+//}
+
+//=>Differnece Between notify and notifyAll()
+//->see picture at 2:36:00
+
+//                          _________
+//          t1-------------|         |-----------t2
+//          t3-------------|  Obj    |-----------t4
+//          t4-------------|_________|-----------t5    
+
+//->multiple  threads acting on object.
+//->if notify calls on object then notification will be sent 
+//  to all the threads, but only one thread will get a chance
+//  for execution,which thread totally depends on Thread sheduler.
+
+//->see picture at 2:45:00
+//1.notify() is used to give norification to one thread. if 
+//  there are multiple waitIng threads, then one thread will 
+//  get a chance, reaming threads should wait for further 
+//  notification but which thread will get a chance it totally 
+//  depends on T.S
+//->But which Thread will be notify(inform) we can't expect 
+//  exactly it depends on JVM.
+
+//	eg: 	waiting state
+//				   |
+//          	   | obj.wait(); 6 threads are waiting
+//    obj.notify() |
+//				   |
+// 	          Running state
+//Among 6 threads which thread will get a chance we don't 
+//have control over that it is decided
+//by JVM(threadscheduler).
+
+//2. notifyAll() is used to give to notification to all 
+//  waiting threads. all waiting threads will be notified 
+//  and it will be exeuted one by one.
+//                        or
+//->We can use notifyAll() method to give the notification
+//  for all waiting Threads of particular object. All waiting 
+//  Threads will be notified and will be executed one by one,
+//  because they required lock.
+
+//->remember train example benguluru  to Belgaum
+//eg:				 waiting state
+//							|
+//							| obj.wait(); 6 threads are waiting
+//		   obj.notifyAll()  | 
+//                          |
+//                     Running state
+
+//Note: On which object we are calling wait(), notify() and 
+//notifyAll() methods that corresponding object lock we have
+//to get but not other object locks.
+
+
+//->When notifyALL() method is called on objects Obj1 and Obj2
+//  all is call for how many thread notification will be send. 
+
+//scenario-1
+//                          _________
+//          t1-------------|         |-----------t2
+//          t3-------------|  Obj1   |-----------t4
+//          t4-------------|_________|-----------t5 
+
+// Obj1.notifyAll()- it gives notification to all threasds. 
+
+//scenario-2
+//                          _________
+//          t1-------------|         |-----------t2
+//          t3-------------|  Obj2   |
+//                         |_________|
+
+
+//=>IllegalMonitorStateException Example
+
+//Example-1 
+//=>we have two stack objects.
+//->if you use s2 inside synchroized block of s1 it result
+//  throws IllegalMonitorStateException
+
+//import java.util.*;
+//
+//public class Multithreading76 {
+//
+//	public static void main(String[] args) throws InterruptedException {
+//		Stack s1=new Stack();
+//		Stack s2=new Stack();
+//		
+//		synchronized(s1){
+//
+//			s2.wait(); //RE: IllegalMonitorStateException
+//
+//		}
+//		
+//	}
+//}
+
+
+//Example-2: valid case
+//->it is allowed use s2 inside the synchronized block of s2.
+import java.util.*;
+
+public class Multithreading76 {
+
+	public static void main(String[] args) throws InterruptedException {
+		Stack s1=new Stack();
+		Stack s2=new Stack();
+		
+//		synchronized(s1){
+
+//			s2.wait(); //(valid)
+
+//		}
+		
+		synchronized(s2){   
+
+			s2.wait();
+
+		}
+	}
+}
+
+//=>Question based on lock
+//1. If a thread calls wait() immediately it will enter into
+//   waiting state without releasing any lock.(false)
+//2. If a thread calls wait() it releases the lock of that 
+//   object but may not immediately (false)
+//3. If a thread calls wait() on any object,it releases all 
+//   locks acquired by that thread and enters into waiting 
+//   state(false).
+//4. If a thread calls wait() on any object,it immediately 
+//   releases the lock of that particular object and entered
+//   into waiting state(true).
+//5. If a thread calls notify() on any object,it immediately releases the lock of
+//   that particular object(invalid)
+//6. If a thread calls notify() on any object,it releases 
+//   the lock of that object but may not immediately(true)
+
